1311abca4bc0407bd9685e0fd34b0df6f874c501
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-06-08 15:59:32 +0900

    gridの座標初期化途中

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -28,6 +28,7 @@ void GameScene::Initialize() {
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 
+
 	//デバッグカメラの生成
 	debugCamera_ = new DebugCamera(1280, 800);
 
@@ -36,15 +37,16 @@ void GameScene::Initialize() {
 	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
 	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 
-	//startMyLine_ = new Vector3(0.0f, 0.0f,0.0f);
 	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
-
 
 }
 
 void GameScene::Update() {
 	//デバッグカメラの更新
 	debugCamera_->Update();
+
+	//Mesh CubePosition = model_.;
+
 }
 
 void GameScene::Draw() {
@@ -70,29 +72,261 @@ void GameScene::Draw() {
 	// 3Dオブジェクト描画前処理
 	Model::PreDraw(commandList);
 
+
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
+	
+
+	Vector3 grid[44]
+		=
+	{
+		/*{-5.0f, 0.0f, -5.0f},
+		{-4.0f, 0.0f, -5.0f},
+		{-3.0f, 0.0f, -5.0f},
+		{-2.0f, 0.0f, -5.0f},
+		{-1.0f, 0.0f, -5.0f},
+		{ 0.0f, 0.0f, -5.0f},
+		{ 1.0f, 0.0f, -5.0f},
+		{ 2.0f, 0.0f, -5.0f},
+		{ 3.0f, 0.0f, -5.0f},
+		{ 4.0f, 0.0f, -5.0f},
+		{ 5.0f, 0.0f, -5.0f},
+
+		{-5.0f, 0.0f, 5.0f},
+		{-4.0f, 0.0f, 5.0f},
+		{-3.0f, 0.0f, 5.0f},
+		{-2.0f, 0.0f, 5.0f},
+		{-1.0f, 0.0f, 5.0f},
+		{ 0.0f, 0.0f, 5.0f},
+		{ 1.0f, 0.0f, 5.0f},
+		{ 2.0f, 0.0f, 5.0f},
+		{ 3.0f, 0.0f, 5.0f},
+		{ 4.0f, 0.0f, 5.0f},
+		{ 5.0f, 0.0f, 5.0f},
+
+
+		{-5.0f, 0.0f, -5.0f},
+		{-5.0f, 0.0f, -4.0f},
+		{-5.0f, 0.0f, -3.0f},
+		{-5.0f, 0.0f, -2.0f},
+		{-5.0f, 0.0f, -1.0f},
+		{-5.0f, 0.0f, 0.0f},
+		{-5.0f, 0.0f, 1.0f},
+		{-5.0f, 0.0f, 2.0f},
+		{-5.0f, 0.0f, 3.0f},
+		{-5.0f, 0.0f, 4.0f},
+		{-5.0f ,0.0f, 5.0f},
+
+		{ 5.0f, 0.0f, -5.0f},
+		{ 5.0f, 0.0f, -4.0f},
+		{ 5.0f, 0.0f, -3.0f},
+		{ 5.0f, 0.0f, -2.0f},
+		{ 5.0f, 0.0f, -1.0f},
+		{ 5.0f, 0.0f, 0.0f},
+		{ 5.0f, 0.0f, 1.0f},
+		{ 5.0f, 0.0f, 2.0f},
+		{ 5.0f, 0.0f, 3.0f},
+		{ 5.0f, 0.0f, 4.0f},
+		{ 5.0f ,0.0f, 5.0f},*/
+
+		//{-5.0f, 0.0f, -5.0f},
+		//{-5.0f, 0.0f, 5.0f},
+
+		//{-4.0f, 0.0f, -5.0f},
+		//{-4.0f, 0.0f, 5.0f},
+
+		//{-3.0f, 0.0f, -5.0f},
+		//{-3.0f, 0.0f, 5.0f},
+
+		//{-2.0f, 0.0f, -5.0f},
+		//{-2.0f, 0.0f, 5.0f},
+
+		//{-1.0f, 0.0f, -5.0f},
+		//{-1.0f, 0.0f, 5.0f},
+
+		//{ 0.0f, 0.0f, -5.0f},
+		//{ 0.0f, 0.0f, 5.0f},
+
+		//{ 1.0f, 0.0f, -5.0f},
+		//{ 1.0f, 0.0f, 5.0f},
+
+		//{ 2.0f, 0.0f, -5.0f},
+		//{ 2.0f, 0.0f, 5.0f},
+
+		//{ 3.0f, 0.0f, -5.0f},
+		//{ 3.0f, 0.0f, 5.0f},
+
+		//{ 4.0f, 0.0f, -5.0f},
+		//{ 4.0f, 0.0f, 5.0f},
+
+		//{ 5.0f, 0.0f, -5.0f},
+		//{ 5.0f, 0.0f, 5.0f},
+
+
+		//{-5.0f, 0.0f, -5.0f},
+		//{ 5.0f, 0.0f, -5.0f},
+
+		//{-5.0f, 0.0f, -4.0f},
+		//{ 5.0f, 0.0f, -4.0f},
+
+		//{-5.0f, 0.0f, -3.0f},
+		//{ 5.0f, 0.0f, -3.0f},
+
+		//{-5.0f, 0.0f, -2.0f},
+		//{ 5.0f, 0.0f, -2.0f},
+
+		//{-5.0f, 0.0f, -1.0f},
+		//{ 5.0f, 0.0f, -1.0f},
+
+		//{-5.0f, 0.0f, 0.0f},
+		//{ 5.0f, 0.0f, 0.0f},
+
+		//{-5.0f, 0.0f, 1.0f},
+		//{ 5.0f, 0.0f, 1.0f},
+
+		//{-5.0f, 0.0f, 2.0f},
+		//{ 5.0f, 0.0f, 2.0f},
+
+		//{-5.0f, 0.0f, 3.0f},
+		//{ 5.0f, 0.0f, 3.0f},
+
+		//{-5.0f, 0.0f, 4.0f},
+		//{ 5.0f, 0.0f, 4.0f},
+
+		//{-5.0f ,0.0f, 5.0f},
+		//{ 5.0f ,0.0f, 5.0f},
+	};
+
+
+	for (int i = 0; i < 44; i++)
+	{
+		if(i < 22)
+		{
+			if(i < 11)
+			{
+				grid[i].x = -5.0f;
+			}
+			else
+			{
+				grid[i].x = 5.0f;
+			}
+
+			/*for (int j = -5; j < 5; j++)
+			{
+				grid[i].z = static_cast<float>(j);
+			}*/
+		}
+		else
+		{
+			if(i < 33)
+			{
+				grid[i].z = -5.0f;
+			}
+			else
+			{
+				grid[i].z = 5.0f;
+			}
+
+			/*for (int j = -5; j < 5; j++)
+			{
+				grid[i].x = static_cast<float>(j);
+			}*/
+		} 
+		grid[i].y = 0.0f;
+	}
+
+	int gridList[22][2] =
+	{
+		/*{0,11},
+		{1,12},
+		{2,13},
+		{3,14},
+		{4,15},
+		{5,16},
+		{6,17},
+		{7,18},
+		{8,19},
+		{9,20},
+		{10,21},
+
+		{22,33},
+		{23,34},
+		{24,35},
+		{25,36},
+		{26,37},
+		{27,38},
+		{28,39},
+		{29,40},
+		{30,41},
+		{31,42},
+		{32,43},*/
+
+
+		/*{0,1},
+		{2,3},
+		{4,5},
+		{6,7},
+		{8,9},
+		{10,11},
+		{12,13},
+		{14,15},
+		{16,17},
+		{18,19},
+		{20,21},
+		{22,23},
+		{24,25},
+		{26,27},
+		{28,29},
+		{30,31},
+		{32,33},
+		{34,35},
+		{36,37},
+		{38,39},
+		{40,41},
+		{42,43},*/
+
+	};
+
+	for (int i = 0; i < 22; i++)
+	{
+		if (i < 11)
+		{
+			gridList[i][0] = i;
+		}
+		else 
+		{
+			gridList[i][0] = i + 11;
+		}
+
+		gridList[i][1] = gridList[i][0] + 11;
+	}
+
+	Vector4 xColor =
+	{
+		0xFF, 0x00, 0x00, 0xFF
+	};
+
+	Vector4 zColor =
+	{
+		0x00, 0x00, 0xFF, 0xFF
+	};
+
 	/// </summary>
 
 	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
-
+		
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 
 	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
 
-	/*for (int i = 0; i < 12; i++)
-	{
-		PrimitiveDrawer::GetInstance()->DrawLine3d(vertex[edgeList[i][0]], vertex[edgeList[i][1]], vecColor);
-	}*/
-
 	for (int i = 0; i < 22; i++)
 	{
-		if(i < 11)
-		PrimitiveDrawer::GetInstance()->DrawLine3d(grid[gridList[i][0]], grid[gridList[i][1]], zColor);
+		if (i < 11)
+			PrimitiveDrawer::GetInstance()->DrawLine3d(grid[gridList[i][0]], grid[gridList[i][1]], zColor);
 
 		else
-		PrimitiveDrawer::GetInstance()->DrawLine3d(grid[gridList[i][0]], grid[gridList[i][1]], xColor);
+			PrimitiveDrawer::GetInstance()->DrawLine3d(grid[gridList[i][0]], grid[gridList[i][1]], xColor);
 	}
 
 #pragma endregion
