0b00d59fc8d844cc8649da6b795aed08bb3ba3c8
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-06-15 12:48:34 +0900

    使わないコメントを処理

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -23,8 +23,6 @@ void GameScene::Initialize() {
 
 	srand(time(NULL));
 
-	
-
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
@@ -72,7 +70,6 @@ void GameScene::Initialize() {
 
 
 		//Rote
-		//float radian = 45 * PI / 180.0;
 		Vector3 radian =
 		{
 			static_cast<float>(randomRote.x * PI / 180.0f),
@@ -163,15 +160,6 @@ void GameScene::Initialize() {
 		worldTransform.TransferMatrix();
 	}
 
-	//カメラ視点座標を設定
-	//viewProjection_.eye = {0,0,-10};
-
-	//カメラ注視点座標を設定
-	//viewProjection_.target = {10,0,0};
-
-	//カメラ上方向ベクトルを設定 (右上45度)
-	//viewProjection_.up = { cosf(PI / 4.0f), sinf(PI / 4.0f), 0.0f };
-
 	//カメラ垂直方向視野を設定
 	viewProjection_.fovAngleY = XMConvertToRadians(10.0f);
 
@@ -193,7 +181,7 @@ void GameScene::Initialize() {
 	//軸方向表示の表示を有効にする
 	AxisIndicator::GetInstance()->SetVisible(true);
 	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
-	AxisIndicator::GetInstance()->SetTargetViewProjection(&viewProjection_ /*&debugCamera_->GetViewProjection()*/);
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&viewProjection_ );
 
 	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
 }
@@ -201,88 +189,6 @@ void GameScene::Initialize() {
 void GameScene::Update() {
 	//デバッグカメラの更新
 	debugCamera_->Update();
-//
-//#pragma region //視点処理
-//	{
-//	//視点移動処理
-//	{
-//		//視点ベクトル
-//		Vector3 move = { 0,0,0 };
-//
-//		//視点の移動速さ
-//		const float kEyeSpeed = 0.2f;
-//
-//		//押した方向で移動ベクトルを変更
-//		if (input_->PushKey(DIK_W)) {
-//			move.z += kEyeSpeed;
-//		}else if (input_->PushKey(DIK_S)) {
-//			move.z -= kEyeSpeed;
-//		}
-//
-//		//視点移動(ベクトルの加算)
-//		viewProjection_.eye += move;
-//
-//		//行列の再計算
-//		viewProjection_.UpdateMatrix();
-//
-//		//デバッグ用表示
-//		debugText_->SetPos(50, 50);
-//		debugText_->Printf(
-//			"eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
-//	}
-//
-//	//注視点移動処理
-//	{
-//		//注視点ベクトル
-//		Vector3 move = { 0,0,0 };
-//
-//		//視点の移動速さ
-//		const float kTargetSpeed = 0.2f;
-//
-//		//押した方向で移動ベクトルを変更
-//		if (input_->PushKey(DIK_LEFT)) {
-//			move.x -= kTargetSpeed;
-//		}else if (input_->PushKey(DIK_RIGHT)) {
-//			move.x += kTargetSpeed;
-//		}
-//
-//		//視点移動(ベクトルの加算)
-//		viewProjection_.target += move;
-//
-//		//行列の再計算
-//		viewProjection_.UpdateMatrix();
-//
-//		//デバッグ用表示
-//		debugText_->SetPos(50, 70);
-//		debugText_->Printf(
-//			"target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y, viewProjection_.target.z);
-//	}
-//
-//	//上方向回転処理
-//	{
-//		//上方向の回転速さ[ラジアン/frame]
-//		const float kUpSpeed = 0.05f;
-//
-//		//押した方向で移動ベクトルを変更
-//		if (input_->PushKey(DIK_SPACE)) {
-//			viewAngle += kUpSpeed;
-//			// 2πを超えたら0に戻す
-//			viewAngle = fmodf(viewAngle, PI * 2.0f);
-//		}
-//
-//		//上方向ベクトルを計算(半径1の円周上の座標)
-//		viewProjection_.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
-//
-//		//行列の再計算
-//		viewProjection_.UpdateMatrix();
-//
-//		//デバッグ用表示
-//		debugText_->SetPos(50, 90);
-//		debugText_->Printf(
-//			"up:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
-//	}
-//	}
-//#pragma endregion 
 
 	//FoV変更処理
 	{
@@ -322,9 +228,6 @@ void GameScene::Update() {
 		debugText_->Printf(
 			"nearZ:%f", XMConvertToDegrees(viewProjection_.nearZ));
 	}
-
-
-	
 }
 
 void GameScene::Draw() {
@@ -357,18 +260,13 @@ void GameScene::Draw() {
 	//範囲forですべてのワールドトランスフォームを順に処理する
 	for (WorldTransform& worldTransform : worldTransforms_)
 	{
-		model_->Draw(worldTransform, viewProjection_ /*debugCamera_->GetViewProjection()*/, textureHandle_);
+		model_->Draw(worldTransform, viewProjection_ , textureHandle_);
 	}
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 
 	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
-
-	//for (int i = 0; i < 12; i++)
-	//{
-	//	PrimitiveDrawer::GetInstance()->DrawLine3d(vertex[edgeList[i][0]], vertex[edgeList[i][1]], vecColor);
-	//}
 
 #pragma endregion
 
