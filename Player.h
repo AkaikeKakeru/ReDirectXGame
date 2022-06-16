6a43ee164b6e953a5a0ef1279113005b790670c1
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-06-16 14:01:28 +0900

    メンバ変数

--- Player.h
+++ Player.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "Model.h"
+#include "WorldTransform.h"
 
 ///<summary>
 ///自キャラ
@@ -25,5 +27,10 @@ public:
 	void Draw();
 
 private:
-
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
 };
