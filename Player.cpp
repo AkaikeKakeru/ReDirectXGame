734dfc865e47b8f3f3b0e3f7b2c31dc24a89c433
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-06-16 13:56:24 +0900

    Playerクラスの型を生成

---
+++ Player.cpp
@@ -0,0 +1,27 @@
+#include <Player.h>
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+///<summary>
+///‰Šú‰»
+///<summary>
+void Player::Intialize() {
+}
+
+///<summary>
+///XV
+///<summary>
+void Player::Update(){
+}
+
+///<summary>
+///•`‰æ
+///<summary>
+void Player::Draw(){
+}
---
+++ Player.h
@@ -0,0 +1,29 @@
+#pragma once
+
+///<summary>
+///©ƒLƒƒƒ‰
+///<summary>
+class Player
+{
+public:
+	Player();
+	~Player();
+
+	///<summary>
+	///‰Šú‰»
+	///<summary>
+	void Intialize();
+
+	///<summary>
+	///XV
+	///<summary>
+	void Update();
+
+	///<summary>
+	///•`‰æ
+	///<summary>
+	void Draw();
+
+private:
+
+};
