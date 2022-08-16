36255ed98c877a38823b53031bf6be7cad829b8b
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-16 16:46:01 +0900

    弾の座標に速度を足して、座標を移動させる

--- PlayerBullet.cpp
+++ PlayerBullet.cpp
@@ -30,6 +30,9 @@ void PlayerBullet::Intialize(Model* model, const Vector3& position, const Vector
 ///更新
 ///<summary>
 void PlayerBullet::Update(){
+	//座標を移動させる(1フレーム分の移動量を足しこむ)
+	worldTransform_.translation_ += velocity_;
+
 	Transfer(worldTransform_,myMatrix_);
 }
 
