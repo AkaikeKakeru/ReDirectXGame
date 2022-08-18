3711888dbbd71ceb5ed2ba85e5de095f186997d0
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-18 16:38:37 +0900

    Enemyクラスに、基本的なメンバ関数とメンバ変数を追加

--- Enemy.h
+++ Enemy.h
@@ -1,4 +1,9 @@
 #pragma once
+
+#include "Model.h"
+#include "WorldTransform.h"
+#include "DebugText.h"
+
 /// <summary>
 /// 敵
 /// </summary>
@@ -7,8 +12,31 @@ class Enemy
 public:
 	Enemy();
 	~Enemy();
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	/// /// <param name="velocity">速度</param>
+	void Intialize(Model* mode, const Vector3& position, const Vector3& velocity);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void UpDate();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション(参照渡し)</param>
+	void Draw(const ViewProjection& viewProjection);
 
 private:
-
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
 };
 
