e36b7d888010be295e77e17c0281a31967e913fa
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-24 22:58:27 +0900

    EnemyBulletに基本的な関数を作成

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -1,9 +1,22 @@
 #include"EnemyBullet.h"
 
-EnemyBullet::EnemyBullet()
-{
-}
+EnemyBullet::EnemyBullet(){}
+EnemyBullet::~EnemyBullet(){}
 
-EnemyBullet::~EnemyBullet()
-{
-}
+///<summary>
+///初期化
+///<summary>
+/// <param name="model">モデル</param>
+/// <param name="position">初期座標</param>
+void EnemyBullet::Intialize(Model* mode, const Vector3& position) {}
+
+///<summary>
+///更新
+///<summary>
+void EnemyBullet::Update(){}
+
+///<summary>
+///描画
+///<summary>
+/// <param name="viewProjection">ビュープロジェクション(参照渡し)</param>
+void EnemyBullet::Draw(const ViewProjection& viewProjection){}
--- EnemyBullet.h
+++ EnemyBullet.h
@@ -4,5 +4,24 @@ class EnemyBullet{
 public:
 	EnemyBullet();
 	~EnemyBullet();
+
+	///<summary>
+	///初期化
+	///<summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	void Intialize(Model* mode, const Vector3& position);
+
+	///<summary>
+	///更新
+	///<summary>
+	void Update();
+
+	///<summary>
+	///描画
+	///<summary>
+	/// <param name="viewProjection">ビュープロジェクション(参照渡し)</param>
+	void Draw(const ViewProjection& viewProjection);
+
 private:
 };
