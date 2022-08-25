cb2720f44d546b8f3fc6c4ce266bb29da545bbe5
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-25 15:28:08 +0900

    TimedCallクラスを定義

---
+++ TimedCall.cpp
@@ -0,0 +1,18 @@
+#include "TimedCall.h"
+
+TimedCall::TimedCall(std::function<int(void)> f,uint32_t time) {
+	f_ = f;
+	time_ = time;
+};
+
+void TimedCall::Update() {
+	if (isFinished_ == true) {
+		return;
+	}
+	time_--;
+	if (time_ <= 0) {
+		isFinished_ = true;
+		//コールバック関数呼び出し
+		(f_)();
+	}
+}
