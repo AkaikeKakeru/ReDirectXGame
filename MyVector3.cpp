e896a5b403dca729d61f4aa1bab309da74084aef
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-26 22:00:17 +0900

    線形補間関数を作成

--- MyVector3.cpp
+++ MyVector3.cpp
@@ -2,6 +2,11 @@
 
 Vector3 MyVector3::Lerp(const Vector3& v1, const Vector3& v2, float t) {
 	Vector3 result;
+	//Vector3 a = v2 - v1;
+	//Vector3 b = t * a;
+	//result = v1 + b;
+
+	result = v1 + (t * (v2 - v1));
 
 	return result;
 };
