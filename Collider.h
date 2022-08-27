09ac5f0b756094e435135fe17f9bfb5a0603a52b
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-27 19:07:03 +0900

    Colliderに、ワールド座標を取得するゲッターを、純粋仮想関数として宣言。

--- Collider.h
+++ Collider.h
@@ -8,15 +8,14 @@ public:
 	//衝突時に呼ばれる関数
 	virtual void OnCollision();
 
+	//ワールド座標を取得
+	virtual Vector3 GetWorldPosition() = 0;
 	//半径を取得
 	float GetRadius();
+
 	//半径を設定
 	void SetRadius(float r) {this->radius_ = r; };
 private:
 	//衝突半径
 	float radius_ = 0.5f;
 };
-
-
-//ワールド座標を取得
-virtual Vector3 GetWorldPosition() = 0;
