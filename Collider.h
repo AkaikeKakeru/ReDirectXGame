0e538036beb4d1cb090a1cf09f19a93444b651f1
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-28 01:12:56 +0900

    Collider.hに衝突属性を持たせる

--- Collider.h
+++ Collider.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vector3.h"
+#include <cstdint>
 
 /// <summary>
 /// 衝突判定オブジェクト
@@ -13,10 +14,23 @@ public:
 	virtual Vector3 GetWorldPosition() = 0;
 	//半径を取得
 	virtual float GetRadius() = 0;
+	//衝突属性(自分)を取得
+	virtual uint32_t GetCollisionAttribute() = 0;
+	//衝突マスク(相手)を取得
+	virtual uint32_t GetCollisionMask() = 0;
 
 	//半径を設定
 	void SetRadius(float r) {this->radius_ = r; };
+	//衝突属性(自分)を設定
+	void SetCollisionAttribute(uint32_t collisionAttribute) { this->collisionAttribute_ = collisionAttribute; };
+	//衝突マスク(相手)を設定
+	void SetCollisionMask(uint32_t collisionMask) { this->collisionMask_ = collisionMask; };
 private:
 	//衝突半径
 	float radius_ = 0.5f;
+
+	//衝突属性(自分)
+	uint32_t collisionAttribute_ = 0xffffffff;
+	//衝突マスク(相手)
+	uint32_t collisionMask_ = 0xffffffff;
 };
