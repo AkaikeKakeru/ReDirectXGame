c599951494d9510c95797b1f4bd51b32d7305f6b
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-28 19:48:37 +0900

    CollisionManagerに、GameSceneの当たり判定処理と、コライダーリストに登録するセッターと、コライダーリストをクリアする関数を追加。

--- CollisionManager.cpp
+++ CollisionManager.cpp
@@ -1,3 +1,51 @@
-class CollisionManager {
+#include "CollisionManager.h"
 
+// 衝突判定と応答
+void CollisionManager::CheckAllCollision() {
+	//リスト内のペアを総当たり
+	std::list<Collider*>::iterator itrA = colliders_.begin();
+	for (; itrA != colliders_.end(); ++itrA){
+		Collider* colA = *itrA;
+
+		//イテレータBはイテレータAの次の要素から回す(重複判定を回避)
+		std::list<Collider*>::iterator itrB = itrA;
+		itrB++;
+		for (; itrB != colliders_.end(); ++itrB){
+			Collider* colB = *itrB;
+
+			//ペアの衝突判定
+			CheckCollisionPair(colA,colB);
+		}
+	}
+};
+
+// コライダー2つの衝突判定と応答
+void CollisionManager::CheckCollisionPair(Collider* colliderA, Collider* colliderB) {
+	//衝突フィルタリング
+	if (
+		(colliderA->GetCollisionAttribute() & colliderB->GetCollisionMask()) == 0 ||
+		(colliderB->GetCollisionAttribute() & colliderA->GetCollisionMask()) == 0
+		){
+		return;
+	}
+
+	//コライダーのワールド座標を取得
+	Vector3 posA = colliderA->GetWorldPosition();
+	Vector3 posB = colliderB->GetWorldPosition();
+
+	float rA = colliderA->GetRadius();
+	float rB = colliderB->GetRadius();
+
+	if (myVector3_.CollisionAlgorithm(posA, rA, posB, rB) == true) {
+
+		//自弾の衝突時コールバックを呼び出す
+		colliderA->OnCollision();
+		//敵弾の衝突時コールバックを呼び出す
+		colliderB->OnCollision();
+	}
+};
+
+/// コライダーリストを全削除
+void CollisionManager::ClearColliderList() {
+	colliders_.clear();
 };
--- CollisionManager.h
+++ CollisionManager.h
@@ -1,1 +1,38 @@
 #pragma once
+#include "MyVector3.h"
+#include <memory>
+#include "Collider.h"
+#include <list>
+
+class CollisionManager {
+public:
+	/// <summary>
+	/// 衝突判定と応答
+	/// </summary>
+	void CheckAllCollision();
+
+	/// コライダーリストを全削除
+	void ClearColliderList();
+
+	/// <summary>
+	/// セッター
+	/// </summary>
+	//衝突マスク(相手)を設定
+	void SetCollider (Collider* collider) { //コライダーをリストに登録
+		colliders_.push_back(collider); };
+
+private:
+	/// <summary>
+	/// コライダー2つの衝突判定と応答
+	/// </summary>
+	/// <param name="colliderA">コライダーA</param>
+	/// <param name="colliderB">コライダーB</param>
+	void CheckCollisionPair(Collider* colliderA,Collider* colliderB);
+
+
+	//コライダーリスト
+	std::list<Collider*> colliders_;
+
+	//Vector3による特殊な計算の関数まとめ
+	MyVector3 myVector3_;
+};
