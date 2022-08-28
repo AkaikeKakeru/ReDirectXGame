5563e57ac68f85783c49ae8bed40b9a39bbcc982
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-08-28 23:34:02 +0900

    skydomeに基本的な関数を作成。

---
+++ skydome.cpp
@@ -0,0 +1,16 @@
+#include "skydome.h"
+
+// ‰Šú‰»
+void skydome::Initialize() {
+
+};
+
+// –ˆƒtƒŒ[ƒ€ˆ—
+void skydome::Update(){
+
+};
+
+// •`‰æ
+void skydome::Draw(){
+
+};
--- skydome.h
+++ skydome.h
@@ -4,5 +4,19 @@
 /// “V‹…
 /// </summary>
 class skydome {
+public:
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize();
 
+	/// <summary>
+	/// –ˆƒtƒŒ[ƒ€ˆ—
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// •`‰æ
+	/// </summary>
+	void Draw();
 };
